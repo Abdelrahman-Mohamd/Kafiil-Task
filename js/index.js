fde320c5f2812afee43ad8a10febce96332f35f7
Abdelrahman Mohamed <abdelrahman181181@yahoo.com>
2024-02-05 23:00:12 +0200

    finished the Navbar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
-const avatar = document.querySelector(".avatar-items");
+const avatar = document.querySelector(".avatar");
 const avatarPopup = document.querySelector(".navbar-popup");
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const hamburgerNavItems = document.querySelector(".hamburger-nav-bar");
+const xIcon = document.querySelector(".x-icon");
 
 function togglePopup() {
   if (
@@ -7,9 +10,45 @@ function togglePopup() {
     avatarPopup.style.display === ""
   ) {
     avatarPopup.style.display = "block";
+    hamburgerNavItems.style.display = "none"; // Close hamburgerNavItems when opening avatarPopup
   } else {
     avatarPopup.style.display = "none";
   }
 }
 
+function toggleHamburgerNavItems() {
+  if (
+    hamburgerNavItems.style.display === "none" ||
+    hamburgerNavItems.style.display === ""
+  ) {
+    hamburgerNavItems.style.display = "block";
+    avatarPopup.style.display = "none";
+  } else {
+    hamburgerNavItems.style.display = "none";
+  }
+}
+
+hamburgerNavItems.style.display = "none";
+
 avatar.addEventListener("click", togglePopup);
+
+window.addEventListener("click", (event) => {
+  if (
+    !avatar.contains(event.target) &&
+    !avatarPopup.contains(event.target) &&
+    !hamburgerIcon.contains(event.target) &&
+    !hamburgerNavItems.contains(event.target)
+  ) {
+    avatarPopup.style.display = "none";
+    hamburgerNavItems.style.display = "none";
+  }
+});
+
+hamburgerIcon.addEventListener("click", toggleHamburgerNavItems);
+
+xIcon.addEventListener("click", () => {
+  hamburgerNavItems.style.display = "none";
+});
+window.addEventListener("resize", () => {
+  hamburgerNavItems.style.display = "none";
+});
